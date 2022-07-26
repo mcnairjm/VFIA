e5474b2990df40f70b7c97d9ab9d2c24a68b6735
mcnairjm19 <mcnairjm1993@gmail.com>
2022-07-26 15:29:56 -0400

    finished slideshow

--- frontend/src/Components/Slide.js
+++ frontend/src/Components/Slide.js
@@ -1,24 +1,33 @@
 import React from 'react';
 import 'react-slideshow-image/dist/styles.css';
 import { Slide } from 'react-slideshow-image';
-import cart from '../Assets/Images/cart.jpg';
-import employee from '../Assets/Images/employee.jpg';
-import wine from '../Assets/Images/wine-guy.jpg';
-import woman from '../Assets/Images/woman-shopper.jpg';
 
 
 const Slideshow = () => {
     const images = [
-        "https://github.com/mcnairjm/VFIA/blob/b97257f52c5a97752cdc85459cf9ba1c2d26de72/frontend/src/Assets/Images/cart1.jpg?raw=true",
+        "https://github.com/mcnairjm/VFIA/blob/335edd3950fcdaae6dbf39e5516a94d61beb90bf/frontend/src/Assets/Images/cart1.jpg?raw=true",
         "https://github.com/mcnairjm/VFIA/blob/b97257f52c5a97752cdc85459cf9ba1c2d26de72/frontend/src/Assets/Images/employee1.jpg?raw=true",
         "https://github.com/mcnairjm/VFIA/blob/b97257f52c5a97752cdc85459cf9ba1c2d26de72/frontend/src/Assets/Images/wine-guy1.jpg?raw=true",
         "https://github.com/mcnairjm/VFIA/blob/b97257f52c5a97752cdc85459cf9ba1c2d26de72/frontend/src/Assets/Images/woman-shopper1.jpg?raw=true"
     ];
 
+    const buttonStyle = {
+        width: "30px",
+        background: 'none',
+        border: '0px',
+    };
+
+    const properties = {
+        prevArrow: <button className='prevArrow' style={{ ...buttonStyle }}><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" fill="#fff"><path d="M242 180.6v-138L0 256l242 213.4V331.2h270V180.6z"/></svg></button>,
+        nextArrow: <button className='nextArrow' style={{ ...buttonStyle }}><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" fill="#fff"><path d="M512 256L270 42.6v138.2H0v150.6h270v138z"/></svg></button>
+    }
+
     return (
-        <Slide>
+        <Slide {...properties}>
             <div className="each-slide-effect">
                 <div style={{ 'backgroundImage': `url(${images[0]})` }}>
+                    <span className='span'>For Grocers.</span>
+                    <span className='span1'>For Virginia.</span>
                 </div>
             </div>
             <div className="each-slide-effect">
