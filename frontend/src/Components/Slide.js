40bbb709fd54d2c879831e7981ccd68655d92a67
mcnairjm19 <mcnairjm1993@gmail.com>
2022-07-26 10:50:34 -0400

    new images, beginning slideshow

---
+++ frontend/src/Components/Slide.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import 'react-slideshow-image/dist/styles.css';
+import { Slide } from 'react-slideshow-image;'
+
+const slideImages = [
+    {
+        url: '../images/cart.jpg',
+        caption: 'Slide 1 - Shopping Cart'
+    },
+    {
+        url: '../images/employee.jpg',
+        caption: 'Slide 2 - Employee'
+    },
+    {
+        url: '../images/wine-guy.jpg',
+        caption: 'Slide 3 - Wine Shopper'
+    },
+    {
+        url: '../images/woman-shopper',
+        caption: 'Slide 4 - Woman Shopper'
+    }
+]
+
+
+const Hero = () => {
+    return (
+        <div className='heroContent'>
+         
+        </div>
+    )
+}
+
+export default Hero;
