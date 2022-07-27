775d61168eda5d9e5e71015f2eaee09ff57317f0
mcnairjm19 <mcnairjm1993@gmail.com>
2022-07-27 17:01:16 -0400

    edits

--- frontend/src/Components/Section.js
+++ frontend/src/Components/Section.js
@@ -1,20 +1,20 @@
 import React from 'react';
-import logo from '../Assets/Images/logo.jpg';
+import vfia from '../Assets/Images/vfia.png';
 import MLA from '../Assets/Images/MLA.jpg';
 
 const Section = () => {
     return (
         <div>
             <div className='section-content' id='section1'>
-                <img src={logo}
-                     className='full-logo'/>
-                <div className='info'>
-                    <h1 className='title'>Who We Are</h1>
-                    <p className='text'>The Virginia Food Industry Association (VFIA) was formed to be the leading advocate for retail and wholesale food industries in the commonwealth of Virginia. With members representing more than 55,000 people statewide at more than 530 retail locations, the association provides resources and public affairs support to the food industry as its members work to serve their customers and nourish local communities across the commonwealth.  </p>
+                <div className='image'>
+                    <img src={vfia}
+                        className='full-logo'/>
                 </div>
+                <h1 className='title'>Our Mission</h1>
+                
             </div>
 
-            <div className='section-content' id='section2' >
+           {/*  <div className='section-content' id='section2' >
                 <img src={MLA} 
                     className='MLA'/>
                 <div className='mla-info'>
@@ -28,7 +28,7 @@ const Section = () => {
             <div className='section-content' id='section3'>
                 <h1 className='header'>Reach Out</h1>
                 <p className='text'></p>
-            </div>
+            </div> */}
         </div>
 
     );
