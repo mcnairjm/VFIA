4db60648d4e2eae329f0c068aa34de17063a9c38
mcnairjm19 <mcnairjm1993@gmail.com>
2022-09-14 15:18:37 -0400

    fotter

--- frontend/src/Components/Footer.js
+++ frontend/src/Components/Footer.js
@@ -5,6 +5,16 @@ const Footer = () => {
     return (
         <>  
         <Container fluid className='section4'>
+            <address className='address'>
+                PO Box 318 <br />
+                Richmond, VA 23218 <br />
+                <a href='mailto:MelissaA@VAFoodIndustry.org'>MelissaA@VAFoodIndustry.org</a><br />
+                608-216-5226
+            </address>
+
+            <div className='copy'>
+                &copy; 2022 VA Food Industry Association
+            </div>
 
         </Container>
         </>
