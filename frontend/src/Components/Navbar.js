4b6b179639364dd50f5f5be9cf6e9cef17a79934
mcnairjm19 <mcnairjm1993@gmail.com>
2022-09-27 16:52:56 -0400

    refactor navbar for better responsiveness

--- frontend/src/Components/Navbar.js
+++ frontend/src/Components/Navbar.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
-import full from '../Assets/Images/full.png';
+import smaller from '../Assets/Images/smaller.png';
 import { Link, animateScroll as scroll } from 'react-scroll';
-import Nav from 'react-bootstrap/Nav';
+import { Nav, Navbar } from 'react-bootstrap';
 
 export default class NavBar extends Component {
     scrollToTop = () => {
@@ -11,7 +11,99 @@ export default class NavBar extends Component {
     render() {
 
     return (
-    <>
+        <>
+            <Navbar className='nav' bg='white' sticky='top' expand='lg' collapseOnSelect>
+                <Navbar.Brand>
+                <img
+                    src={smaller}
+                    
+                    className='nav-logo'
+                    alt='logo'
+                    onClick={this.scrollToTop}
+                />
+                </Navbar.Brand>
+
+                <Navbar.Toggle className='toggle'/>
+
+                <Navbar.Collapse className='tags'>
+                <Nav.Link className='navLinks'>
+                    <Link 
+                            eventKey='1'
+                            className='link'
+                            activeClass='active'
+                            to='section1'
+                            spy={true}
+                            smooth={true}
+                            offset={-70}
+                            duration={250}
+                        > What We Do
+                    </Link>
+                    <Link
+                            eventKey='2'
+                            className='link'
+                            activeClass='active'
+                            to='section2'
+                            spy={true}
+                            smooth={true}
+                            offset={-70}
+                            duration={500}
+                        >
+                            Who We Are
+                    </Link>
+                    <Link
+                            eventKey='3'
+                            className='link'
+                            activeClass='active'
+                            to='section3'
+                            spy={true}
+                            smooth={true}
+                            offset={-70}
+                            duration={500}
+                        >
+                            Reach Out
+                    </Link>
+                </Nav.Link>
+                </Navbar.Collapse>
+                {/* <Nav className='navLinks'>
+                    <Link 
+                            className='link'
+                            activeClass='active'
+                            to='section1'
+                            spy={true}
+                            smooth={true}
+                            offset={-70}
+                            duration={250}
+                        > What We Do
+                    </Link>
+                    <Link
+                            className='link'
+                            activeClass='active'
+                            to='section2'
+                            spy={true}
+                            smooth={true}
+                            offset={-70}
+                            duration={500}
+                        >
+                            Who We Are
+                    </Link>
+                    <Link
+                            className='link'
+                            activeClass='active'
+                            to='section3'
+                            spy={true}
+                            smooth={true}
+                            offset={-70}
+                            duration={500}
+                        >
+                            Reach Out
+                    </Link>
+                </Nav> */}
+            </Navbar>
+        </>
+    )
+    }
+};
+    {/* <>
          <Nav className='justify-content-center' activeKey='/home' id='navbar'>
             <Nav.Item className='nav-content'>
                 <img
@@ -67,7 +159,5 @@ export default class NavBar extends Component {
                 </ul>
             </Nav.Item>
         </Nav> 
-    </>
-    )
-    }
-}
+    </> */}
+    
