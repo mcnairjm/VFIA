18430b2a5b4a7fb5a9c880733b1e9e650be1702a
mcnairjm19 <mcnairjm1993@gmail.com>
2022-11-01 14:36:29 -0400

    heroku

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,14 @@ const nodemailer = require('nodemailer');
 
 dotenv.config();
 
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+app.listen(9000);
+
 const app = express();
 app.use(cors());
 app.use(express.json());
